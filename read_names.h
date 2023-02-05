adcdebe03034878a1bef99c02738813d71f51605
dangkyle <dangkyle@csu.fullerton.edu>
2023-02-04 20:02:56 -0800

    adding read_names header

---
+++ read_names.h
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string.h>
+class read_names {
+
+    string names [4];
+    // pull the names from the readme file and then save it into an array
+    read_names();
+
+    // return the names to main
+    get_names() {
+
+        return names[];
+
+    }
+
+
+}
