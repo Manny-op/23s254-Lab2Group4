bbf4e1e1da52a1c404ec526c75288d8004578146
Emmanuel Salcedo <emsalcedo@csu.fullerton.edu>
2023-02-07 13:16:39 -0800

    manage to have implementatio to read from the README.md file and in terminal, spits out the names of the members

--- Lab2.cpp
+++ Lab2.cpp
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <math.h>
 #include <string>
+#include "read.hpp"
 
 
 
@@ -12,8 +13,8 @@ int main()
     //create array of strings
     string fullTeam[4];
     //call the function from read.cpp to read each of the members names
-
+    membernames = readReadMe();
     //print out each members name in the array
-
+    cout << membernames << " "  << endl;
     return 0;
 }
---
+++ read.hpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include <string>
+#include <stdio.h>
+#include <fstream>
+#include <istream>
+#include <ostream>
+
+std::string readReadMe();
