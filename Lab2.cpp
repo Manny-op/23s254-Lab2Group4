9edb353161939b3e0225e4098d14dc027accb4f7
saul2009 <saulandrade124@gmail.com>
2023-02-07 22:45:40 -0800

    open README.md and use functions from .h file to do work

--- Lab2.cpp
+++ Lab2.cpp
@@ -1,13 +1,24 @@
-#include <stdio.h>
-#include <math.h>
-
-
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <sstream>
+#include "read_names.h"
 
 using namespace std;
 
 int main()
 {
-
+    //open file
+    ifstream myfile;
+    myfile.open("README.md",ios::in);
+    //if file is open read names and print 
+    if(myfile.is_open())
+    {
+        while(read_names())
+        {
+            cout << get_names();
+        }
+    }
 
     return 0;
 }
